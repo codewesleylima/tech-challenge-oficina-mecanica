29043b0a26e0ca6ce2a191118c5801267e60c3d6
codewesleylima <gusterlima@hotmail.com>
2026-04-27 23:56:23 -0300

    first commit

---
+++ src/test/java/com/safiap/techchallengeoficinamecanica/TechChallengeOficinaMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safiap.techchallengeoficinamecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeOficinaMecanicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
