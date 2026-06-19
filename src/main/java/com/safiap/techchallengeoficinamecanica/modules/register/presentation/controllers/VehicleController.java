bedbd8bf9e63d053bb8e285abbb559125d6d63ad
matpitas <matheus4pitas@gmail.com>
2026-06-19 01:10:48 -0300

    feat(presentation): Add controller and dtos

---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/presentation/controllers/VehicleController.java
@@ -0,0 +1,11 @@
+package com.safiap.techchallengeoficinamecanica.modules.register.presentation.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+
+}
