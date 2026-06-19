7f9aa2138997cca5e45ed767819cceea0942a485
matpitas <matheus4pitas@gmail.com>
2026-06-19 01:24:58 -0300

    refactor(presentation): refactor vehicle presentation

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/presentation/controllers/VehicleController.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/presentation/controllers/VehicleController.java
@@ -1,5 +1,12 @@
 package com.safiap.techchallengeoficinamecanica.modules.register.presentation.controllers;
 
+import com.safiap.techchallengeoficinamecanica.modules.register.application.commands.AddVehicleCommand;
+import com.safiap.techchallengeoficinamecanica.modules.register.application.responses.vehicle.AddVehicleResponse;
+import com.safiap.techchallengeoficinamecanica.modules.register.application.use_cases.AddVehicleUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vehicles")
 public class VehicleController {
 
+    private final AddVehicleUseCase addVehicleUseCase;
+
+    public VehicleController(AddVehicleUseCase addVehicleUseCase) {
+        this.addVehicleUseCase = addVehicleUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<AddVehicleResponse> addVehicle(
+            @RequestBody AddVehicleCommand request) {
+
+        AddVehicleCommand command = new AddVehicleCommand(
+                request.customerId(),
+                request.carLicensePlate(),
+                request.model(),
+                request.manufacturer(),
+                request.Kilometers(),
+                request.year()
+        );
+
+        AddVehicleResponse response = addVehicleUseCase.execute(command);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    };
 
 }
