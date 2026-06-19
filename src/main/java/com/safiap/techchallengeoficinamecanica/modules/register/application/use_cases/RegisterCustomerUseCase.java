678d2bcd01a47a31aa75deab94d82886df51ba21
matpitas <matheus4pitas@gmail.com>
2026-06-19 01:09:23 -0300

    feat(application): Add use-cases, commands and responses

---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/application/use_cases/RegisterCustomerUseCase.java
@@ -0,0 +1,36 @@
+package com.safiap.techchallengeoficinamecanica.modules.register.application.use_cases;
+
+import com.safiap.techchallengeoficinamecanica.modules.register.application.commands.RegisterCustomerCommand;
+import com.safiap.techchallengeoficinamecanica.modules.register.domain.entities.Customer;
+import com.safiap.techchallengeoficinamecanica.modules.register.domain.repositories.CustomerRepository;
+import com.safiap.techchallengeoficinamecanica.modules.register.application.responses.customer.RegisterCustomerResponse;
+import com.safiap.techchallengeoficinamecanica.modules.register.domain.value_objects.CPF;
+import com.safiap.techchallengeoficinamecanica.modules.register.domain.value_objects.Email;
+import com.safiap.techchallengeoficinamecanica.modules.register.domain.value_objects.Phone;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterCustomerUseCase {
+
+    private final CustomerRepository customerRepository;
+
+    public RegisterCustomerUseCase(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public RegisterCustomerResponse execute(RegisterCustomerCommand request) {
+
+        Customer customer = Customer.createCustomer(
+                request.name(),
+                new Email(request.email()),
+                new Phone(request.phone()),
+                new CPF(request.cpf())
+        );
+
+        customerRepository.save(customer);
+
+        return new RegisterCustomerResponse(customer.getName());
+
+    }
+
+}
