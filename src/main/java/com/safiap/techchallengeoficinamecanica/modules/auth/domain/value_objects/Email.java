82236ea2a1ad5a13d2bc9c08b13a35b48298532e
Tim Morgenstern Beck <69176907+timbeck97@users.noreply.github.com>
2026-06-22 21:16:02 -0300

    fix(domain): fix email validation

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/auth/domain/value_objects/Email.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/auth/domain/value_objects/Email.java
@@ -10,12 +10,14 @@ public record Email(String value) {
             Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
 
     public Email {
-        if (value == null || value.isBlank()) {
+        if (value == null) {
+            throw new DomainException("Email cannot be null or empty.");
+        }
+        value = value.trim().toLowerCase();
+        if (value.isBlank()) {
             throw new DomainException("Email cannot be null or empty.");
         }
         if (!EMAIL_PATTERN.matcher(value).matches()) {
             throw new DomainException("Invalid email format.");
         }
-        value = value.trim().toLowerCase();
-    }
 }
