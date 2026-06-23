e49cca40dcb1c53a48586ef6135f6100a1615300
codewesleylima <gusterlima@hotmail.com>
2026-06-22 22:31:13 -0300

    feat(service-order): add ServiceOrderStatus value object

---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/domain/value_objects/ServiceOrderStatus.java
@@ -0,0 +1,11 @@
+package com.safiap.techchallengeoficinamecanica.modules.serviceorder.domain.value_objects;
+
+public enum ServiceOrderStatus {
+    OPEN,
+    DIAGNOSIS,
+    AWAITING_AUTHORIZATION,
+    AUTHORIZED,
+    IN_EXECUTION,
+    CONCLUDED,
+    CANCELLED
+}
