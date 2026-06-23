c6605d45d9fb5231f0c2713655015102fda20c77
Tim Morgenstern Beck <69176907+timbeck97@users.noreply.github.com>
2026-06-22 21:14:49 -0300

    fix(infraestructure): fix http code autentication error

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/shared/exceptions/AuthException.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/shared/exceptions/AuthException.java
@@ -1,6 +1,10 @@
 package com.safiap.techchallengeoficinamecanica.modules.shared.exceptions;
 
-public class AuthException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthException extends RuntimeException {
     public AuthException(String message) {
         super(message);
     }
