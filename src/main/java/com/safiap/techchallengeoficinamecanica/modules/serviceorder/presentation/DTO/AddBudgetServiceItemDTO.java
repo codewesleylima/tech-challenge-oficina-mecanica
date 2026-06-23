77106336f1dbdd42750a87f50d27e36c0ac4aacb
codewesleylima <gusterlima@hotmail.com>
2026-06-22 22:45:15 -0300

    feat(service-order): add AddBudgetServiceItemDTO

---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/presentation/DTO/AddBudgetServiceItemDTO.java
@@ -0,0 +1,5 @@
+package com.safiap.techchallengeoficinamecanica.modules.serviceorder.presentation.DTO;
+
+import java.math.BigDecimal;
+
+public record AddBudgetServiceItemDTO(String description, BigDecimal price) {}
