98253f050dfaabb4961aabf8b2eda23bcb105400
codewesleylima <gusterlima@hotmail.com>
2026-06-24 16:19:17 -0300

    feat(service-order): expose POST /service-orders on ServiceOrderController

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/presentation/controllers/ServiceOrderController.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/presentation/controllers/ServiceOrderController.java
@@ -1,11 +1,17 @@
 package com.safiap.techchallengeoficinamecanica.modules.serviceorder.presentation.controllers;
 
+import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.commands.OpenServiceOrderCommand;
 import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.responses.ServiceOrderResponse;
 import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.use_cases.GetServiceOrderByIdUseCase;
 import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.use_cases.ListServiceOrdersByCustomerUseCase;
+import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.use_cases.OpenServiceOrderUseCase;
+import com.safiap.techchallengeoficinamecanica.modules.serviceorder.presentation.DTO.OpenServiceOrderDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +22,26 @@ import java.util.UUID;
 @RequestMapping("/service-orders")
 public class ServiceOrderController {
 
+    private final OpenServiceOrderUseCase openServiceOrderUseCase;
     private final GetServiceOrderByIdUseCase getServiceOrderByIdUseCase;
     private final ListServiceOrdersByCustomerUseCase listServiceOrdersByCustomerUseCase;
 
-    public ServiceOrderController(GetServiceOrderByIdUseCase getServiceOrderByIdUseCase,
+    public ServiceOrderController(OpenServiceOrderUseCase openServiceOrderUseCase,
+                                  GetServiceOrderByIdUseCase getServiceOrderByIdUseCase,
                                   ListServiceOrdersByCustomerUseCase listServiceOrdersByCustomerUseCase) {
+        this.openServiceOrderUseCase = openServiceOrderUseCase;
         this.getServiceOrderByIdUseCase = getServiceOrderByIdUseCase;
         this.listServiceOrdersByCustomerUseCase = listServiceOrdersByCustomerUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<ServiceOrderResponse> openServiceOrder(@RequestBody OpenServiceOrderDTO request) {
+        OpenServiceOrderCommand command = new OpenServiceOrderCommand(
+                request.customerId(),
+                request.vehicleId(),
+                request.problemDescription()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(openServiceOrderUseCase.execute(command));
     }
 
     @GetMapping("/{serviceOrderId}")
