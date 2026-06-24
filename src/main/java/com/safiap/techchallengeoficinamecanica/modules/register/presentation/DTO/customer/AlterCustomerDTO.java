b49836f467c3231a012981af27cabe2a5070bce2
matpitas <matheus4pitas@gmail.com>
2026-06-24 02:26:55 -0300

    feat(presentation): add dto for update and list

---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/presentation/DTO/customer/AlterCustomerDTO.java
@@ -0,0 +1,4 @@
+package com.safiap.techchallengeoficinamecanica.modules.register.presentation.DTO.customer;
+
+public record AlterCustomerDTO(String Name, String Email, String Phone, String CPF) {
+}
---
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/register/presentation/DTO/customer/PartialAlterCustomerDTO.java
@@ -0,0 +1,4 @@
+package com.safiap.techchallengeoficinamecanica.modules.register.presentation.DTO.customer;
+
+public record PartialAlterCustomerDTO(String Name, String Email, String Phone, String CPF) {
+}
