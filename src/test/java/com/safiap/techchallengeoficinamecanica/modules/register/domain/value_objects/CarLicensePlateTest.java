9a05f2866b88401fafd1c0aa17b8ba9e7fc3c252
codewesleylima <gusterlima@hotmail.com>
2026-06-27 18:50:29 -0300

    test(register): add CarLicensePlate value object unit tests

---
+++ src/test/java/com/safiap/techchallengeoficinamecanica/modules/register/domain/value_objects/CarLicensePlateTest.java
@@ -0,0 +1,31 @@
+package com.safiap.techchallengeoficinamecanica.modules.register.domain.value_objects;
+
+import com.safiap.techchallengeoficinamecanica.modules.shared.exceptions.DomainException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarLicensePlateTest {
+
+    @Test
+    void acceptsMercosulFormat() {
+        CarLicensePlate plate = new CarLicensePlate("ABC1D23");
+        assertThat(plate.plate()).isEqualTo("ABC1D23");
+    }
+
+    @Test
+    void rejectsOldFormat() {
+        assertThatThrownBy(() -> new CarLicensePlate("ABC1234")).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    void rejectsLowercase() {
+        assertThatThrownBy(() -> new CarLicensePlate("abc1d23")).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    void rejectsNull() {
+        assertThatThrownBy(() -> new CarLicensePlate(null)).isInstanceOf(DomainException.class);
+    }
+}
