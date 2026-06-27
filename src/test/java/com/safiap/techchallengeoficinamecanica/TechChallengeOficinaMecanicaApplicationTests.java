d5d0407074664f7ad5d66cdd5f03d609433f5d4c
codewesleylima <gusterlima@hotmail.com>
2026-06-27 19:05:24 -0300

    test(config): add display name to context load test

--- src/test/java/com/safiap/techchallengeoficinamecanica/TechChallengeOficinaMecanicaApplicationTests.java
+++ src/test/java/com/safiap/techchallengeoficinamecanica/TechChallengeOficinaMecanicaApplicationTests.java
@@ -1,5 +1,6 @@
 package com.safiap.techchallengeoficinamecanica;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 class TechChallengeOficinaMecanicaApplicationTests {
 
     @Test
+    @DisplayName("teste carrega o contexto da aplicação com sucesso")
     void contextLoads() {
     }
 
