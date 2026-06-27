b21c6f0de256b28f3e891060fd56a08d13a8a4fd
codewesleylima <gusterlima@hotmail.com>
2026-06-27 19:05:23 -0300

    test(register): add display names and drop comments in Phone tests

--- src/test/java/com/safiap/techchallengeoficinamecanica/modules/register/domain/value_objects/PhoneTest.java
+++ src/test/java/com/safiap/techchallengeoficinamecanica/modules/register/domain/value_objects/PhoneTest.java
@@ -1,6 +1,7 @@
 package com.safiap.techchallengeoficinamecanica.modules.register.domain.value_objects;
 
 import com.safiap.techchallengeoficinamecanica.modules.shared.exceptions.DomainException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,22 +10,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PhoneTest {
 
     @Test
+    @DisplayName("teste remove caracteres não numéricos do telefone")
     void stripsNonDigits() {
         Phone phone = new Phone("(11) 99999-8888");
         assertThat(phone.value()).isEqualTo("11999998888");
     }
 
     @Test
+    @DisplayName("teste aceita telefone com dez dígitos")
     void acceptsTenDigits() {
         assertThat(new Phone("1133334444").value()).isEqualTo("1133334444");
     }
 
     @Test
+    @DisplayName("teste rejeita telefone curto demais")
     void rejectsTooShort() {
         assertThatThrownBy(() -> new Phone("9999")).isInstanceOf(DomainException.class);
     }
 
     @Test
+    @DisplayName("teste rejeita telefone em branco")
     void rejectsBlank() {
         assertThatThrownBy(() -> new Phone(" ")).isInstanceOf(DomainException.class);
     }
