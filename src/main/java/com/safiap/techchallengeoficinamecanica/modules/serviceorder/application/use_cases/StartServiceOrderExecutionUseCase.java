900bbc45f44a478e01011772b52c2e6af78c0b56
codewesleylima <gusterlima@hotmail.com>
2026-06-29 03:39:55 -0300

    feat(observability): debug log execution start transition

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/application/use_cases/StartServiceOrderExecutionUseCase.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/serviceorder/application/use_cases/StartServiceOrderExecutionUseCase.java
@@ -4,6 +4,8 @@ import com.safiap.techchallengeoficinamecanica.modules.serviceorder.application.
 import com.safiap.techchallengeoficinamecanica.modules.serviceorder.domain.entities.ServiceOrder;
 import com.safiap.techchallengeoficinamecanica.modules.serviceorder.domain.repositories.ServiceOrderRepository;
 import com.safiap.techchallengeoficinamecanica.modules.shared.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,8 @@ import java.util.UUID;
 
 @Service
 public class StartServiceOrderExecutionUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(StartServiceOrderExecutionUseCase.class);
 
     private final ServiceOrderRepository serviceOrderRepository;
 
@@ -25,6 +29,8 @@ public class StartServiceOrderExecutionUseCase {
 
         serviceOrder.startExecution();
         serviceOrderRepository.save(serviceOrder);
+
+        log.debug("Service order {} budget approved - execution started", serviceOrderId);
 
         return ServiceOrderResponse.from(serviceOrder);
     }
