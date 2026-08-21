077e7e237b041590ed62011bdfe4b3b4cc9e5b35
Tim Morgenstern <tim@infisc.com.br>
2026-08-20 22:38:41 -0300

    chore(auth): log login attempt.

--- src/main/java/com/safiap/techchallengeoficinamecanica/modules/auth/presentation/controllers/AuthController.java
+++ src/main/java/com/safiap/techchallengeoficinamecanica/modules/auth/presentation/controllers/AuthController.java
@@ -12,6 +12,9 @@ import com.safiap.techchallengeoficinamecanica.modules.auth.application.use_case
 import com.safiap.techchallengeoficinamecanica.modules.auth.presentation.dto.RegisterAccountDto;
 import com.safiap.techchallengeoficinamecanica.modules.auth.presentation.dto.UserLoginDto;
 import com.safiap.techchallengeoficinamecanica.modules.auth.presentation.dto.AddUserDto;
+import com.safiap.techchallengeoficinamecanica.modules.shared.presentation.GlobalExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +26,7 @@ public class AuthController {
     private final RegisterUserUseCase registerUserUseCase;
     private final RegisterAccountUseCase registerAccountUseCase;
     private final LoginUseCase loginUseCase;
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(RegisterUserUseCase registerUserUseCase, RegisterAccountUseCase registerAccountUseCase, LoginUseCase loginUseCase) {
         this.registerUserUseCase = registerUserUseCase;
@@ -43,6 +47,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<UserTokenResponse> login(@RequestBody UserLoginDto dto){
+        log.info("1 - User login: {}", dto.email());
         return ResponseEntity.ok(loginUseCase.execute(new UserLoginCommand(dto.email(), dto.password())));
     }
 }
